86a32af4c6266bc97bd0d2116263bd5695b75422
Jefferson <jefparra31@gmail.com>
2025-04-11 20:38:47 -0500

    Quiz

---
+++ src/test/java/com/example/QuizArquiEjercicio1/QuizArquiEjercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuizArquiEjercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizArquiEjercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
